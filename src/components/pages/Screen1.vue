865892172eae2589e4eb12bf495645e466b263f4
NUCAL <beritanbasoglu@gmail.com>
2023-04-11 14:07:35 +0300

    NUCAL-UI | Front-end application initial commit, powered by NUCAL development team.

---
+++ src/components/pages/Screen1.vue
@@ -0,0 +1,81 @@
+<template>
+  <DropZone ncBgType="bgColor" backgroundImage="" id="c64bc5c4-3e55-4eea-9118-671479674738"
+    v-bind:style="{'width':'100%','height':'auto'}">
+    <LottieChristmasCelebration src="https://assets9.lottiefiles.com/packages/lf20_8o0tj3yf.json" v-bind:loop="false"
+      v-bind:count="0" v-bind:hover="false" v-bind:speed="1" v-bind:autoplay="true" v-bind:direction="1"
+      v-bind:openCount="false" background="transparent" ncTooltipText="" position="relative"
+      id="f4645b2e-ce26-4b49-b525-d2ebcf9c8fde"
+      v-bind:style="{'width':'100%','height':'100%','marginTop':'0px','marginLeft':'0px','paddingTop':'0px','visibility':'visible','borderColor':'#000000','borderStyle':'none','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px','zIndex':2}"/>
+  </DropZone>
+</template>
+<script>
+export default {
+  name: 'Screen1',
+  data() {
+    return {};
+  },
+  props: {},
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@media (min-width: 1024.05px){[id='c64bc5c4-3e55-4eea-9118-671479674738'] {
+             display: grid; 
+             grid-template-columns: 47.998046875rem 31.25rem 20.751953125rem;
+             grid-template-rows: minmax(27.48046875rem, max-content) minmax(23.4375rem, max-content) minmax(31.89453125rem, max-content);
+          }
+[id='f4645b2e-ce26-4b49-b525-d2ebcf9c8fde']{
+          grid-area: 2/2/3/3;
+          
+          
+          }}
+
+@media (min-width: 768.05px) and (max-width: 1024px){[id='c64bc5c4-3e55-4eea-9118-671479674738'] {
+             display: grid; 
+             grid-template-columns: 30.46875rem 39.0625rem 30.46875rem;
+             grid-template-rows: minmax(34.3505859375rem, max-content) minmax(29.296875rem, max-content) minmax(3.125rem, max-content);
+          }
+[id='f4645b2e-ce26-4b49-b525-d2ebcf9c8fde']{
+          grid-area: 2/2/3/3;
+          
+          
+          }}
+
+@media (min-width: 480.05px) and (max-width: 768px){[id='c64bc5c4-3e55-4eea-9118-671479674738'] {
+             display: grid; 
+             grid-template-columns: 23.958333333333332rem 52.083333333333336rem 23.958333333333332rem;
+             grid-template-rows: minmax(45.80078125rem, max-content) minmax(39.0625rem, max-content) minmax(4.166666667rem, max-content);
+          }
+[id='f4645b2e-ce26-4b49-b525-d2ebcf9c8fde']{
+          grid-area: 2/2/3/3;
+          
+          
+          }}
+
+@media (min-width: 375.05px) and (max-width: 480px){[id='c64bc5c4-3e55-4eea-9118-671479674738'] {
+             display: grid; 
+             grid-template-columns: 5rem 90rem 5rem;
+             grid-template-rows: minmax(73.28125rem, max-content) minmax(67.5rem, max-content) minmax(5rem, max-content);
+          }
+[id='f4645b2e-ce26-4b49-b525-d2ebcf9c8fde']{
+          grid-area: 2/2/3/3;
+          
+          
+          }}
+
+@media (min-width: 0px) and (max-width: 375px){[id='c64bc5c4-3e55-4eea-9118-671479674738'] {
+             display: grid; 
+             grid-template-columns: 6.4rem 87.2rem 6.4rem;
+             grid-template-rows: minmax(93.8rem, max-content) minmax(65.4rem, max-content) minmax(6.4rem, max-content);
+          }
+[id='f4645b2e-ce26-4b49-b525-d2ebcf9c8fde']{
+          grid-area: 2/2/3/3;
+          
+          
+          }}
+</style>
